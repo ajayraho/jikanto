11aab5d0a78a52170567eab51dcba2cf0b7d8491
ajayraho <ajayraho@ymail.com>
2024-05-26 18:30:24 +0530

    EXPCalculation, QuestCompletion, Materia embellishments, scrollbar-hide

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from 'react';
-import { Fire, Add, Aero, Ice, Coin, Shine, UnCheck, Check, Edit, Delete, Hourglass, Dumbbell, Book, Meditation, Water, Japanese, BronzeI, BronzeII, BronzeIII, Tick } from '../components/SVG'
+import * as Icons from '../components/SVG'
 import toast, { Toaster, resolveValue  } from 'react-hot-toast';
 
 import { Description, Dialog, DialogPanel, DialogTitle, Transition, TransitionChild, Button, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
@@ -15,6 +15,8 @@ export default function Home() {
   const [questTimeTo, setQuestTimeTo] = useState("");
   const [today, setToday] = useState({});
   const [tasks, setTasks] = useState([]);
+  const [materiaSelect, setMateriaSelect] = useState("Select Materia");
+  const [EXP, setEXP] = useState(999)
   useEffect(()=>{
     const now = new Date()
     setToday({
@@ -30,7 +32,7 @@ export default function Home() {
         to: "0800",
         isCompleted: false,
         exp: 100,
-        materia:0  
+        materia:1  
       },
       {
         title:"Quest 2",
@@ -46,7 +48,7 @@ export default function Home() {
         to: "1100",
         isCompleted: false,
         exp: 100,
-        materia:0  
+        materia:2  
       },
       {
         title:"Quest 4",
@@ -66,17 +68,73 @@ export default function Home() {
       }     
     ])
   }, []);
+  const materia = [
+    {id:0, name:["Fire","Fira","Firaga"], level:2},
+    {id:1, name:["Blizzard", "Blizzara", "Blizzaga"], level:1},
+    {id:2, name:["Aero", "Aira", "Airaga"], level:0},
+    {id:3, name:["Japanese"], level:0},
+  ]
+  const getMateriaColor=(level)=>{
+    let color="#00effb"
+    if(level===1) color="yellow"
+    else if(level===2) color="red"
+    return color
+  }
+  const materiaList = materia.map(({id,name,level})=>{
+    const Component = Icons[name[0]];
+    return (
+      <MenuItem key={id} onClick={()=>setMateriaSelect(name[level])}>
+        <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
+          <Component h={14} c={getMateriaColor(level)} className="glowing" />
+          <span className="ml-3">{name[level]}</span>
+          {/*<kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘E</kbd>*/}
+        </button>
+      </MenuItem>
+    )
+  })
+  const gainExp = (exp, catalyser, gained) => {
+    //catalyser.level
+    const gainLoss = gained?1:-1;
+    const newExp=(exp * (1*0.1))
+    const strGainedLost=gained===true?'gained':'lost' 
+    let str = 'You '+ strGainedLost +' '+newExp+'XP'
 
-  function onRemove(itemToRemove) {
+    const Component = Icons[GetMateriaFromId(catalyser)]
+
+
+    toast(str,{icon:<Component c={getMateriaColor(materia[catalyser].level)} />})
+    setEXP(EXP + (gainLoss*newExp))
+
+  }
+  const GetMateriaFromId=(id)=>{
+    let materia="Fire";
+    switch(id){
+      case 0:
+        materia="Fire"
+        break
+      case 1:
+        materia="Blizzard"
+        break
+      case 2:
+        materia="Aero"
+        break
+    }
+    return materia
+  }
+  const onRemove = (itemToRemove) => {
     let prevTask = tasks
     for(let i of prevTask)
       if(i===itemToRemove) i.isCompleted=!i.isCompleted
 
     setTasks([...prevTask])
+
+    gainExp(itemToRemove.exp, itemToRemove.materia, itemToRemove.isCompleted)
   }
   console.log(tasks)
+
+
   return (
-    <main className="flex flex-col  mainContent h-screen">
+    <main className="flex flex-col scrollbar-hide overflow-x-hidden  mainContent h-screen">
       <Toaster
         position="bottom-right"
         reverseOrder={false}
@@ -130,6 +188,7 @@ export default function Home() {
                   </DialogTitle>
                   <div className="mt-2 text-sm/6 text-white mt-3">
                     <table className="w-full">
+                    <tbody>
                       <tr>
                         <th className="w-1/2 text-right pr-2 text-[#00effb] font-normal">Quest Title</th>
                         <td className="w-1/2 pl-2"><input type="text" value={questTitle} onChange={(v)=>setQuestTitle(v.target.value)} className="hover:outline-none text-center bg-transparent inputText"/></td>
@@ -143,11 +202,11 @@ export default function Home() {
                         </td>
                       </tr>
                       <tr>
-                        <th className="w-1/2 text-right pr-2 text-[#00effb] font-normal">Badge</th>
+                        <th className="w-1/2 text-right pr-2 text-[#00effb] font-normal">Catalyser Materia</th>
                         <td className="w-40 flex items-center justify-center flex-row pl-2 h-16">
                           <Menu>
                             <MenuButton className="flex items-center gap-2 w-full py-1.5 px-3 text-sm text-white text-center focus:outline-none data-[focus]:outline-1 data-[focus]:outline-white" style={{background:"linear-gradient(90deg, rgba(51,146,204,0) -5%, #01040d 50%, rgba(0,15,36,0) 105%)"}}>
-                              <div className="w-5/6">Options</div>
+                              <div className="w-5/6">{materiaSelect}</div>
                               <div className="w-1/6">v</div>
                             </MenuButton>
                             <Transition
@@ -162,40 +221,15 @@ export default function Home() {
                                 anchor="bottom end"
                                 className="w-52 z-50 origin-top-right border border-white/5 p-1 text-sm text-white [--anchor-gap:var(--spacing-1)] focus:outline-none bg-black" style={{borderRadius:0,zIndex:100,backgroundColor:"rgba(0,0,0,0.8)"}}
                               >
-                                <MenuItem>
-                                  <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
-                                    <Fire h={10} className="size-4 fill-white/30" />
-                                    Fire
-                                    <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘E</kbd>
-                                  </button>
-                                </MenuItem>
-                                <MenuItem>
-                                  <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
-                                    <Aero h={10} className="size-4 fill-white/30" />
-                                    Aero
-                                    <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘D</kbd>
-                                  </button>
-                                </MenuItem>
-                                <div className="my-1 h-px bg-white/5" />
-                                <MenuItem>
-                                  <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
-                                    <Fire h={10} className="size-4 fill-white/30" />
-                                    Archive
-                                    <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘A</kbd>
-                                  </button>
-                                </MenuItem>
-                                <MenuItem>
-                                  <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
-                                    <Fire h={10} className="size-4 fill-white/30" />
-                                    Delete
-                                    <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘D</kbd>
-                                  </button>
-                                </MenuItem>
+                              { 
+                                materiaList
+                              }
                               </MenuItems>
                             </Transition>
                           </Menu>
                         </td>
                       </tr>
+                      </tbody>
                     </table>
                   </div>
                   <div className="mt-6 flex flex-col gap-2 justify-center">
@@ -247,18 +281,18 @@ export default function Home() {
               </div>
                 <div className="flex mt-1 flex-col">
                   <h3 className="text-lg">Bronze I</h3>
-                  <h3 className="text-lg">999XP</h3>
+                  <h3 className="text-lg">{EXP}XP</h3>
                 </div>
               {/*<div onClick={()=>toast('You gained 400XP',{icon:<Fire c="#fff" />})}>
                 350XP
               </div>*/}
             </div>
           </div>
-          <div className="mt-8 overflow-y-auto overflow-x-hidden" style={{height:"360px"}}>
+          <div className="mt-8 flex flex-col gap-y-1 overflow-y-auto overflow-x-hidden" style={{height:"360px"}}>
             {
               tasks.length>0?tasks.map((i,j)=>(
                 
-                  <div key={j} className={`flex px-2 py-2 flex-row ${j%2!==0?"bg-[#05224b]":""} ${i.isCompleted&&"select-none"}`} style={{"height":"60px",  filter: i.isCompleted&&"grayscale(80%)"}}>
+                  <div key={j} className={`flex px-2 py-2 flex-row ${j%2!==0?"bg-[#05224b]":""} ${i.isCompleted&&"select-none"}`} style={{"height":"60px", transition: "all 0.5s ease", filter: i.isCompleted ? "grayscale(80%)" : "grayscale(0%)", background: i.isCompleted && "rgba(26,31,40,0.7)"}}>
                       <div className="w-2/5 flex flex-row overflow-x-hidden">
                         <div className="">{i.from} - {i.to}</div>
                       </div>
@@ -266,12 +300,14 @@ export default function Home() {
                         <h2 className="text-[#00effb] truncate">{i.title}</h2>
                         <div className="text-sm text-right flex justify-between gap-3">
                           <div className="text-xs">
-                            {i.isCompleted&&"Quest Completed"}
+                            {i.isCompleted&&
+                              <span style={{transition: "all 0.5s ease", opacity: i.isCompleted ? "1" : "0"}}>Quest Completed</span>
+                            }
                           </div>
                           <div className="flex flex-row w-1/4 justify-between">
-                            <Delete />
-                            <Edit />
-                            <Tick onClick={() => onRemove(i)} />
+                            <Icons.Delete />
+                            <Icons.Edit />
+                            <Icons.Tick onClick={() => onRemove(i)} />
                           </div>
                         </div>
                       </div>
@@ -290,13 +326,13 @@ export default function Home() {
               <div>
                 <div className="flex gap-3">
                   <h3>Flags</h3>
-                  <div className="flex items-center -mt-1"><Edit h={14} c="#fff"/></div>
+                  <div className="flex items-center -mt-1"><Icons.Edit h={14} c="#fff"/></div>
                 </div>
                 <div className="flex flex-row gap-3 ml-3">
                   <div className="flex flex-row gap-1">
                     
                   </div>
-                  <Add/>
+                  <Icons.Add/>
                 </div>
               </div>
               <div className="mt-3">
@@ -313,7 +349,7 @@ export default function Home() {
                       background: "linear-gradient(to left, #00acc9 90%, rgba(255,0,0,0))",
                     },
                     className: '',
-                    icon: <Hourglass c="#fff" />,
+                    icon: <Icons.Hourglass c="#fff" />,
 
                     // Change colors of success/error/loading icon
                     iconTheme: {
@@ -327,30 +363,30 @@ export default function Home() {
                       'aria-live': 'polite',
                     },
                   })}>Badges</h3>
-                  <div className="flex items-center -mt-1"><Edit h={14} c="#fff"/></div>
+                  <div className="flex items-center -mt-1"><Icons.Edit h={14} c="#fff"/></div>
                 </div>
                 <div className="flex flex-row gap-3 ml-3 mt-2">
-                  <div className="flex items-center drop-shadow-lg -mt-1"><Coin h={16}/></div>
-                  <div className="flex items-center drop-shadow-lg -mt-1"><Shine h={16}/></div>
-                  <div className="flex items-center drop-shadow-lg -mt-1"><Fire h={16}/></div>
-                  <div className="flex items-center drop-shadow-lg -mt-1"><Ice h={16}/></div>
-                  <div className="flex items-center drop-shadow-lg -mt-1"><Aero h={16}/></div>
-                  <div className="flex items-center drop-shadow-lg -mt-1"><Hourglass h={16}/></div>
-                  <div className="flex items-center drop-shadow-lg -mt-1"><Dumbbell h={16}/></div>
-                  <div className="flex items-center drop-shadow-lg -mt-1"><Book h={16}/></div>
-                  <div className="flex items-center drop-shadow-lg -mt-1"><Meditation h={16}/></div>
-                  <div className="flex items-center drop-shadow-lg -mt-1"><Water h={16}/></div>
-                  <div className="flex items-center drop-shadow-lg -mt-1"><Japanese h={16}/></div>
-                  <Add/>
+                  <div className="flex items-center drop-shadow-lg -mt-1"><Icons.Coin h={16}/></div>
+                  <div className="flex items-center drop-shadow-lg -mt-1"><Icons.Shine h={16}/></div>
+                  <div className="flex items-center drop-shadow-lg -mt-1"><Icons.Fire h={16}/></div>
+                  <div className="flex items-center drop-shadow-lg -mt-1"><Icons.Blizzard h={16}/></div>
+                  <div className="flex items-center drop-shadow-lg -mt-1"><Icons.Aero h={16}/></div>
+                  <div className="flex items-center drop-shadow-lg -mt-1"><Icons.Hourglass h={16}/></div>
+                  <div className="flex items-center drop-shadow-lg -mt-1"><Icons.Dumbbell h={16}/></div>
+                  <div className="flex items-center drop-shadow-lg -mt-1"><Icons.Book h={16}/></div>
+                  <div className="flex items-center drop-shadow-lg -mt-1"><Icons.Meditation h={16}/></div>
+                  <div className="flex items-center drop-shadow-lg -mt-1"><Icons.Water h={16}/></div>
+                  <div className="flex items-center drop-shadow-lg -mt-1"><Icons.Japanese h={16}/></div>
+                  <Icons.Add/>
                 </div>
                 <div className="flex flex-row gap-3 ml-3 mt-2">
-                  <div className="flex items-center drop-shadow-lg -mt-1"><BronzeI h={20}/></div>
-                  <div className="flex items-center drop-shadow-lg -mt-1"><BronzeII h={20}/></div>
-                  <div className="flex items-center drop-shadow-lg -mt-1"><BronzeIII h={20}/></div>
+                  <div className="flex items-center drop-shadow-lg -mt-1"><Icons.BronzeI h={20}/></div>
+                  <div className="flex items-center drop-shadow-lg -mt-1"><Icons.BronzeII h={20}/></div>
+                  <div className="flex items-center drop-shadow-lg -mt-1"><Icons.BronzeIII h={20}/></div>
                 </div>
               </div>
             </div>
-            <div className="w-2/12 flex justify-end content-end items-end">
+            <div className="fixed right-5 w-2/12 flex justify-end content-end items-end">
               <div className="flex text-xl drop-shadow-lg cursor-pointer justify-center items-center bg-[#081829] w-10 h-10"
 
                  onClick={() => {
